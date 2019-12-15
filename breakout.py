169b512f723f1596d63468a5c46d8449608d6303
joshtrigger <lugjosh@gmail.com>
2019-12-15 20:36:19 +0300

    initial commit

---
+++ breakout.py
@@ -0,0 +1,16 @@
+class BreakOut:
+    def __init__(self):
+        pass
+
+    def start(self): pass
+    def change_scene(self): pass
+    def get_level(self): pass
+    def get_score(self): pass
+    def increase_score(self): pass
+    def get_lives(self): pass
+    def get_balls(self): pass
+    def get_pad(self): pass
+    def play_sound(self, clip): pass
+    def reduce_lives(self): pass
+    def increase_lives(self): pass
+    def reset(self): pass
